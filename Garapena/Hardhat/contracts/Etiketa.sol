06f5759d244873947242cdda7b07d99cbe1af4ea
aiza-fp <aiza@fpzornotza.com>
2024-05-06 22:00:30 +0200

    Contrato con índices en eventos y función para obtener los lotes creados

--- Garapena/Hardhat/contracts/Etiketa.sol
+++ Garapena/Hardhat/contracts/Etiketa.sol
@@ -11,9 +11,11 @@ contract Etiketa {
     address private owner;
     // Mapping to store forms using a uint256 identifier, renamed from id to lote
     mapping(uint256 => Form) private forms;
+    // Array to store all lote identifiers
+    uint256[] private loteList;
 
-    event FormCreated(uint256 lote, string publicData, string privateData);
-    event FormUpdated(uint256 lote, string publicData, string privateData);
+    event FormCreated(uint256 indexed lote, string publicData, string privateData);
+    event FormUpdated(uint256 indexed lote, string publicData, string privateData);
 
     constructor() {
         owner = msg.sender; // Set the contract deployer as the owner
@@ -28,18 +30,35 @@ contract Etiketa {
     function createForm(uint256 _lote, string memory _publicData, string memory _privateData) public onlyOwner {
         require(bytes(forms[_lote].publicData).length == 0, "Form with this lote already exists");
         forms[_lote] = Form(_publicData, _privateData);
+        loteList.push(_lote);
         emit FormCreated(_lote, _publicData, _privateData);
     }
 
     function updateForm(uint256 _lote, string memory _publicData, string memory _privateData) public onlyOwner {
         require(bytes(forms[_lote].publicData).length != 0, "Form does not exist");
         forms[_lote] = Form(_publicData, _privateData);
+        if (!isLoteListed(_lote)) {
+            loteList.push(_lote);
+        }
         emit FormUpdated(_lote, _publicData, _privateData);
     }
-    // Function to retrieve form data by its lote
+
     function getForm(uint256 _lote) public view returns (string memory, string memory) {
         require(bytes(forms[_lote].publicData).length != 0, "Form does not exist");
         Form memory form = forms[_lote];
         return (form.publicData, form.privateData);
+    }
+
+    function getLoteList() public view returns (uint256[] memory) {
+        return loteList;
+    }
+
+    function isLoteListed(uint256 _lote) private view returns (bool) {
+        for (uint i = 0; i < loteList.length; i++) {
+            if (loteList[i] == _lote) {
+                return true;
+            }
+        }
+        return false;
     }
 }
