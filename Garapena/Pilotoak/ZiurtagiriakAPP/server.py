4fc034c15d71d5bb87d8327e3180d30b40a8e675
aiza-fp <aiza@fpzornotza.com>
2024-11-26 09:52:43 +0100

    cambios forma de conectarse a la BD

--- Garapena/Pilotoak/ZiurtagiriakAPP/server.py
+++ Garapena/Pilotoak/ZiurtagiriakAPP/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from web3 import Web3
 import os, time
+from contextlib import contextmanager
+import mysql.connector
 
 app = Flask(__name__, template_folder='www', static_url_path='/static')
 contract_addr = os.environ.get('DIRECCION_CONTRATO_ZIURTAGIRIAK')
@@ -10,23 +12,38 @@ sender_email = os.environ.get('SMTP_EMAIL')
 sender_email_password = os.environ.get('SMTP_PASSWORD')
 #to-do: definir el provider dentro de las funciones seleccionando uno cada vez
 
+@contextmanager
 def get_db_connection():
+    """Context manager for database connections to ensure proper cleanup"""
+    connection = None
     max_retries = 5
     retry_delay = 2  # seconds
-    import mysql.connector as con
+    
     for attempt in range(max_retries):
         try:
-            return con.connect(
-                host='database', 
-                database='ziurtagiriak', 
-                user='ziurtagiriak', 
-                password='ziurtagiriak', 
-                autocommit=True
+            connection = mysql.connector.connect(
+                host='database',
+                database='ziurtagiriak',
+                user='ziurtagiriak',
+                password='ziurtagiriak',
+                autocommit=True,
+                # Add these connection parameters
+                connection_timeout=30,
+                pool_size=5,
+                pool_name='mypool',
+                pool_reset_session=True
             )
-        except con.Error as err:
+            break
+        except mysql.connector.Error as err:
             if attempt == max_retries - 1:
                 raise
             time.sleep(retry_delay)
+    
+    try:
+        yield connection
+    finally:
+        if connection and connection.is_connected():
+            connection.close()
 
 @app.route("/")
 def hello_world():
@@ -34,11 +51,14 @@ def hello_world():
 
 @app.get("/jardunaldia/")
 def get_jardunaldia():
-    bbdd = get_db_connection()
-    cursor = bbdd.cursor()
-    query = "SELECT id, izena FROM erakundeak"
-    cursor.execute(query)
-    return render_template("jardunaldia.html", cursor = cursor)
+    with get_db_connection() as bbdd:
+        cursor = bbdd.cursor()
+        try:
+            query = "SELECT id, izena FROM erakundeak"
+            cursor.execute(query)
+            return render_template("jardunaldia.html", cursor=cursor)
+        finally:
+            cursor.close()
 
 @app.post("/jardunaldia/")
 def post_jardunaldia():
@@ -58,103 +78,88 @@ def post_jardunaldia():
     csvFile = request.files['csv'].readlines()
     print(erakundea, emailea, formakuntza, lekua, data)
     #BBDD
-    bbdd = get_db_connection()
-    cursor = bbdd.cursor()
-    query = "INSERT INTO jardunaldiak (iderakundea, emailea, formakuntza, data, lekua) VALUES (%s,%s,%s,%s,%s)"
-    cursor.execute(query, (erakundea, emailea, formakuntza, data, lekua))
-    id_jar = cursor.lastrowid
-    newline = '\n'
-    
-    primera_linea = True
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login(sender_email, sender_email_password)
-        for row in csvFile:
-            if primera_linea:
-                primera_linea = False
-            else:
-                linea = row.decode().split(";")
-                izena = linea[0]
-                receiver_email = linea[1]
-                #receiver_email = "aiza@fpzornotza.com"
-                localizador = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-                query = "INSERT INTO partaideak (izena, emaila, lokalizatzailea, id_jardunaldia) VALUES (%s,%s,%s,%s)"
-                cursor.execute(query, (izena, receiver_email, localizador, id_jar))
-                id_par = cursor.lastrowid
-                email_lok = str(id_jar%100) + "-" + str(localizador) + "-" + str(id_par%100)
-                email_lok = f"{id_jar%100:02d}" + "-" + str(localizador) + "-" + f"{id_par%100:02d}"
-                print("The generated random string : " + email_lok)
+    with get_db_connection() as bbdd:
+        cursor = bbdd.cursor()
+        try:
+            query = "INSERT INTO jardunaldiak (iderakundea, emailea, formakuntza, data, lekua) VALUES (%s,%s,%s,%s,%s)"
+            cursor.execute(query, (erakundea, emailea, formakuntza, data, lekua))
+            id_jar = cursor.lastrowid
+            newline = '\n'
+            
+            primera_linea = True
+            with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+                server.login(sender_email, sender_email_password)
+                for row in csvFile:
+                    if primera_linea:
+                        primera_linea = False
+                    else:
+                        linea = row.decode().split(";")
+                        izena = linea[0]
+                        receiver_email = linea[1]
+                        #receiver_email = "aiza@fpzornotza.com"
+                        localizador = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+                        query = "INSERT INTO partaideak (izena, emaila, lokalizatzailea, id_jardunaldia) VALUES (%s,%s,%s,%s)"
+                        cursor.execute(query, (izena, receiver_email, localizador, id_jar))
+                        id_par = cursor.lastrowid
+                        email_lok = str(id_jar%100) + "-" + str(localizador) + "-" + str(id_par%100)
+                        email_lok = f"{id_jar%100:02d}" + "-" + str(localizador) + "-" + f"{id_par%100:02d}"
+                        print("The generated random string : " + email_lok)
 
-                message = MIMEMultipart("alternative")
-                message["Subject"] = formakuntza + " - Ziurtagiri froga"
-                message["From"] = sender_email
-                message["To"] = receiver_email
-                with open("static/email.html", "r") as f:
-                    mail_html = f.read().replace("{{izena}}", izena).replace("{{email_lok}}", email_lok).replace("{{formakuntza}}", formakuntza)
-                with open("static/email.txt", "r") as f:
-                    mail_text = f.read().replace("{{izena}}", izena).replace("{{email_lok}}", email_lok).replace("{{formakuntza}}", formakuntza)
-                part1 = MIMEText(mail_text, "plain")
-                part2 = MIMEText(mail_html, "html")
+                        message = MIMEMultipart("alternative")
+                        message["Subject"] = formakuntza + " - Ziurtagiri froga"
+                        message["From"] = sender_email
+                        message["To"] = receiver_email
+                        with open("static/email.html", "r") as f:
+                            mail_html = f.read().replace("{{izena}}", izena).replace("{{email_lok}}", email_lok).replace("{{formakuntza}}", formakuntza)
+                        with open("static/email.txt", "r") as f:
+                            mail_text = f.read().replace("{{izena}}", izena).replace("{{email_lok}}", email_lok).replace("{{formakuntza}}", formakuntza)
+                        part1 = MIMEText(mail_text, "plain")
+                        part2 = MIMEText(mail_html, "html")
 
-                # Add HTML/plain-text parts to MIMEMultipart message
-                # The email client will try to render the last part first
-                message.attach(part1)
-                message.attach(part2)
+                        # Add HTML/plain-text parts to MIMEMultipart message
+                        # The email client will try to render the last part first
+                        message.attach(part1)
+                        message.attach(part2)
 
-                
-                # TODO: Send email here
-                server.sendmail(sender_email, receiver_email, message.as_string())
-                
-    cursor.close()
-    bbdd.close()
+                        
+                        # TODO: Send email here
+                        server.sendmail(sender_email, receiver_email, message.as_string())
+                        
+        finally:
+            cursor.close()
     return render_template("jardunaldia.html")
 
 def loka(lokalizatzailea):
     lok = lokalizatzailea.split("-")
     if (len(lok) == 3):
-        bbdd = get_db_connection()
-        cursor = bbdd.cursor()
-        query = """SELECT p.izena, p.emaila, e.izena, j.emailea, j.formakuntza, j.data, j.lekua, p.id
-        FROM partaideak p, jardunaldiak j, erakundeak e
-        WHERE j.id = p.id_jardunaldia AND e.id = j.iderakundea AND %s = p.lokalizatzailea AND %s = p.id%100 AND %s = j.id%100"""
-        cursor.execute(query, (lok[1], int(lok[2]), int(lok[0])))
-        print(cursor)
-        row = cursor.fetchone()
-        if row:
-            """ izena = row[0]
-            emaila = row[1]
-            erakundea = row[2]
-            emailea = row[3]
-            formakuntza = row[4]
-            data = row[5]
-            lekua = row[6] """
-            print("AAAA")
-            cursor.close()
-            bbdd.close()
-            return render_template("ziurtagiria.html",row=row, lok=lokalizatzailea)
-
-        else:
-            cursor.close()
-            bbdd.close()
-            return render_template("lokalizatzailea.html",error="bai")
+        with get_db_connection() as bbdd:
+            cursor = bbdd.cursor()
+            try:
+                query = """SELECT p.izena, p.emaila, e.izena, j.emailea, j.formakuntza, j.data, j.lekua, p.id
+                FROM partaideak p, jardunaldiak j, erakundeak e
+                WHERE j.id = p.id_jardunaldia AND e.id = j.iderakundea AND %s = p.lokalizatzailea AND %s = p.id%100 AND %s = j.id%100"""
+                cursor.execute(query, (lok[1], int(lok[2]), int(lok[0])))
+                row = cursor.fetchone()
+                if row:
+                    return render_template("ziurtagiria.html", row=row, lok=lokalizatzailea)
+                else:
+                    return render_template("lokalizatzailea.html", error="bai")
+            finally:
+                cursor.close()
     else:
-        return render_template("lokalizatzailea.html",error="bai")
-
-    
+        return render_template("lokalizatzailea.html", error="bai")
 
 def ezabatu_ziurtagiria(lokalizatzailea):
     lok = lokalizatzailea.split("-")
-    bbdd = get_db_connection()
-    cursor = bbdd.cursor()
-    query = "DELETE FROM partaideak WHERE %s = lokalizatzailea AND %s = id%100"
-    cursor.execute(query, (lok[1], int(lok[2])))
-    
-    print(cursor)
-    cursor.close()
-    bbdd.close()
+    with get_db_connection() as bbdd:
+        cursor = bbdd.cursor()
+        try:
+            query = "DELETE FROM partaideak WHERE %s = lokalizatzailea AND %s = id%100"
+            cursor.execute(query, (lok[1], int(lok[2])))
+        finally:
+            cursor.close()
     return 0
 
-    
-    
 @app.route('/lokalizatzailea/<lok>')
 def lokalizatzailea(lok):
     return loka(lok)
@@ -207,66 +212,69 @@ def post_sortu_nft_baztertu():
         #path = "http://ziurtagiriak.localhost/static/nft/"
 
         lok = lokalizatzailea.split("-")
-        bbdd = get_db_connection()
-        cursor = bbdd.cursor()
-        query = """SELECT p.izena, p.emaila, e.izena, j.emailea, j.formakuntza, j.data, j.lekua, p.id
-        FROM partaideak p, jardunaldiak j, erakundeak e
-        WHERE j.id = p.id_jardunaldia AND e.id = j.iderakundea AND %s = p.lokalizatzailea AND %s = p.id%100 AND %s = j.id%100"""
-        cursor.execute(query, (lok[1], int(lok[2]), int(lok[0])))
-        ret = ""
-        print(cursor)
-        if cursor:
-            row = cursor.fetchone()
-            ret += str(row[0]) + str(row[1]) + str(row[2]) + str(row[3]) + str(row[4]) + str(row[5]) + str(row[6]) + str(row[7])
-            izena = row[0]
-            emaila = row[1]
-            erakundea = row[2]
-            emailea = row[3]
-            formakuntza = row[4]
-            data = row[5]
-            lekua = row[6]
+        with get_db_connection() as bbdd:
+            cursor = bbdd.cursor()
+            try:
+                query = """SELECT p.izena, p.emaila, e.izena, j.emailea, j.formakuntza, j.data, j.lekua, p.id
+                FROM partaideak p, jardunaldiak j, erakundeak e
+                WHERE j.id = p.id_jardunaldia AND e.id = j.iderakundea AND %s = p.lokalizatzailea AND %s = p.id%100 AND %s = j.id%100"""
+                cursor.execute(query, (lok[1], int(lok[2]), int(lok[0])))
+                
+                row = cursor.fetchone()
+                if row:
+                    ret = str(row[0]) + str(row[1]) + str(row[2]) + str(row[3]) + str(row[4]) + str(row[5]) + str(row[6]) + str(row[7])
+                    izena = row[0]
+                    emaila = row[1]
+                    erakundea = row[2]
+                    emailea = row[3]
+                    formakuntza = row[4]
+                    data = row[5]
+                    lekua = row[6]
+                    
+                    root = minidom.Document()
+                    xml = root.createElement('ziurtagiria') 
+                    xml.setAttribute('lokalizatzailea', lokalizatzailea)
+                    root.appendChild(xml)
+                    xml_formakuntza = root.createElement('formakuntza')
+                    text_form = root.createTextNode(formakuntza)
+                    xml_formakuntza.appendChild(text_form)
+                    xml_erakundea = root.createElement('erakundea')
+                    text_erakundea = root.createTextNode(erakundea)
+                    xml_erakundea.appendChild(text_erakundea)
+                    xml_emailea = root.createElement('emailea')
+                    text_emailea = root.createTextNode(emailea)
+                    xml_emailea.appendChild(text_emailea)
+                    xml_data = root.createElement('data')
+                    text_data = root.createTextNode(data)
+                    xml_data.appendChild(text_data)
+                    xml_lekua = root.createElement('lekua')
+                    text_lekua = root.createTextNode(lekua)
+                    xml_lekua.appendChild(text_lekua)
+                    xml_partaidea = root.createElement('partaidea')
+                    text_partaidea = root.createTextNode(izena)
+                    xml_partaidea.appendChild(text_partaidea)
+                    
+                    xml.appendChild(xml_formakuntza)
+                    xml.appendChild(xml_erakundea)
+                    xml.appendChild(xml_emailea)
+                    xml.appendChild(xml_data)
+                    xml.appendChild(xml_lekua)
+                    xml.appendChild(xml_partaidea)
+                    
+                    xml_str = root.toprettyxml(indent ="\t") 
+                    print(xml_str)
+                    xml_hash = hashlib.sha256(xml_str.encode('utf-8')).hexdigest()
+                    print(xml_hash)
+                    
+                    save_path_file = "static/nft/" + xml_hash + ".xml"
+                    print(save_path_file)
+                    
+                    with open(save_path_file, "w") as f:
+                        f.write(xml_str) 
+                    uri = path+xml_hash+".xml"
 
-            root = minidom.Document()
-            xml = root.createElement('ziurtagiria') 
-            xml.setAttribute('lokalizatzailea', lokalizatzailea)
-            root.appendChild(xml)
-            xml_formakuntza = root.createElement('formakuntza')
-            text_form = root.createTextNode(formakuntza)
-            xml_formakuntza.appendChild(text_form)
-            xml_erakundea = root.createElement('erakundea')
-            text_erakundea = root.createTextNode(erakundea)
-            xml_erakundea.appendChild(text_erakundea)
-            xml_emailea = root.createElement('emailea')
-            text_emailea = root.createTextNode(emailea)
-            xml_emailea.appendChild(text_emailea)
-            xml_data = root.createElement('data')
-            text_data = root.createTextNode(data)
-            xml_data.appendChild(text_data)
-            xml_lekua = root.createElement('lekua')
-            text_lekua = root.createTextNode(lekua)
-            xml_lekua.appendChild(text_lekua)
-            xml_partaidea = root.createElement('partaidea')
-            text_partaidea = root.createTextNode(izena)
-            xml_partaidea.appendChild(text_partaidea)
-            
-            xml.appendChild(xml_formakuntza)
-            xml.appendChild(xml_erakundea)
-            xml.appendChild(xml_emailea)
-            xml.appendChild(xml_data)
-            xml.appendChild(xml_lekua)
-            xml.appendChild(xml_partaidea)
-            
-            xml_str = root.toprettyxml(indent ="\t") 
-            print(xml_str)
-            xml_hash = hashlib.sha256(xml_str.encode('utf-8')).hexdigest()
-            print(xml_hash)
-            
-            save_path_file = "static/nft/" + xml_hash + ".xml"
-            print(save_path_file)
-            
-            with open(save_path_file, "w") as f:
-                f.write(xml_str) 
-            uri = path+xml_hash+".xml"
+            finally:
+                cursor.close()
 
         if web3.is_connected():
             print("Web3 conectado, direccion contrato: ", contract_addr, flush=True)
