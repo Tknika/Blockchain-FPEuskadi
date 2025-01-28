d946f936352bde1b6705b466a66bdba8571fc00c
aiza-fp <aiza@fpzornotza.com>
2025-01-28 13:17:31 +0100

    generar clave privada aleatoria

--- Garapena/Pilotoak/Erremintak/getAddress.py
+++ Garapena/Pilotoak/Erremintak/getAddress.py
@@ -6,6 +6,8 @@ def get_public_address(private_key):
     return account.address
 
 # Example usage
-private_key = "0xe6a413b0e7813bec1877175e1260502405828b53e83a36e5f9df1966821a402d"  # Replace with your private key
+#private_key = "0xe6a413b0e7813bec1877175e1260502405828b53e83a36e5f9df1966821a402d"  # Replace with your private key
+private_key = Web3().eth.account.create().key.hex()
+print(f"Generated private key: {private_key}")
 public_address = get_public_address(private_key)
 print(f"Public address: {public_address}")
